5db1284aadb1749f0b61fd9e64a6d8b18695a36a
Mudassir-razi <54622087+Mudassir-razi@users.noreply.github.com>
2020-06-08 16:54:37 +0600

    Update Node.cpp
    Added input system for two different type of simulation.Also, much of the assigning of variables are now done behind the scene.

--- Node.cpp
+++ Node.cpp
@@ -4,23 +4,31 @@
 #include "helper.h"
 #include "solver.h"
 #include <vector>
-
-using namespace std;
+#include <fstream>
+#include <math.h>
+#include <time.h>
+#include <limits>
 
 //Global variable for storing components
-vector<Nodes> nodes;
-vector<Resistor> res;
-vector<Voltage> vs;
-vector<Current> is;
-vector<Vdvs> vdvs;
+std::vector<double> nodes;
+std::vector<Resistor> res;
+std::vector<Voltage> vs;
+std::vector<Current> is;
+std::vector<Vdvs> vdvs;
+std::vector<Vdcs> vdcs;
+std::vector<Capacitor> caps;
+
+void print_solution(double* sol);
 
 int main()
 {
-    float** a_mat, * z_mat, * sol;
+    double* sol;
+    int sim_type;
+    ofstream out;
+    out.open("output.txt");
 
-    int vol_len, node_len, vdvs_len;
-
-    Helper stream(&nodes, &res, &vs, &is, &vdvs);
+    Helper stream(&nodes, &res, &vs, &is, &vdvs, &vdcs, &caps);
+    clock_t start, stop;
 
     while (true) 
     {
@@ -30,37 +38,66 @@ int main()
         stream.Update(input);
     }
 
-    stream.list_components();
+    while (true)
+    {
+        cout << "Simulation type:\n1.Steady state\n2.Time domain\n";
+        cin >> sim_type;
+        if (sim_type != 1 && sim_type != 2)cout << "Error: Invalid input!!\n";
+        else break;
+    }
+    stream.update_index();
 
-    vol_len = vs.size();
-    node_len = nodes.size();
-    vdvs_len = vdvs.size();
-    int len = vol_len + node_len + vdvs_len - 1;
-    
     //Solution part:
     Matrix mat;
-    Solver solve;
 
-    mat.Toggle_log(true);
-    solve.Toggle_log(true);
-    a_mat= mat.A_matrix(nodes, vs, vdvs);
-    z_mat = mat.Z_matrix(nodes, vs, vdvs);
-
-    sol = solve.solution(a_mat, z_mat, len);
-
-    //Now assigning solutions;
-    cout << "\n..............................................." << endl;
-    for (int i = 1; i < node_len; i++)
+    mat.Toggle_log(false);
+    double time = 0, del_time = 0.0001, end_time = 0.5;
+    
+    if (sim_type == 2)
     {
-        nodes.at(i).node_voltage = sol[i - 1];
-        cout << "Node " << i << " voltage: " << sol[i - 1] <<"v"<< endl;
+        cout << "start time    End time    time step\n";
+        cin >> time >> end_time>> del_time;
+        start = clock();
+        while (time < end_time)
+        {
+            out << time << "  " << caps.at(0).voltage_accross << endl;
+            sol = mat.transient(stream, del_time);
+            time += del_time;
+            delete[] sol;
+        }
+    }
+    else if (sim_type == 1)
+    {
+        stream.list_components();
+        start = clock();
+        sol = mat.Solution_generator(stream);
+        print_solution(sol);
+        delete[] sol;
     }
 
-    for (int i = 0; i < vol_len; i++)
-    {
-        vs.at(i).current_through = -sol[i + node_len-1];
-        cout << "Source " << vs.at(i).name << " current: " << -sol[i +node_len-1] <<"A"<< endl;
-    }
+    stop = clock();
 
+    cout << "\n\nSimulation time: " << stop - start <<" mili seconds"<<endl;
     return 0;
+}
+
+void print_solution(double* sol)
+{
+    int len = nodes.size();
+    int shift = 0;
+    cout << "\n..............................................." << endl;
+    for (int i = 1; i < len; i++)
+    {
+        nodes.at(i) = sol[i - 1];
+        cout << "Node " << i << " voltage: " << sol[i - 1] << "v" << endl;
+    }
+
+    shift = len;
+    len = vs.size();
+
+    for (int i = 0; i < len; i++)
+    {
+        vs.at(i).current_through = -sol[i + shift - 1];
+        cout << "Source " << vs.at(i).name << " current: " << -sol[i + shift - 1] << "A" << endl;
+    }
 }
