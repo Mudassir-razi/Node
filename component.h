65afd0a5d65d5fd439a52c39425a1095819f6089
Mudassir-razi <54622087+Mudassir-razi@users.noreply.github.com>
2020-06-08 16:57:43 +0600

    Update component.h
    How components works has changed vastly.Earlier, the system needed change when adding new element.Now, we just have to decide the behavior of each component to the matrix.Meaning, we don't need to change the matrix system each time we add new components.

--- component.h
+++ component.h
@@ -2,26 +2,33 @@
 #include<iostream>
 #include<string>
 #include<vector>
+#include<iomanip>
+#include <limits>
 
 using namespace std;
 
 //This is the base class of all components.
 //coz they have same attributes.two terminals and one value
+//Solution sequence is:
+// 1.Node_voltages -> 2.current_through_independent_v_sources -> 3.current_through_dependent_v_sources -> 4.dv/dt_of_capacitors
 
 class Component
 {
 public:
 
-	float value;	//primary value of the component
+	string name;
+	double value;	//primary value of the component
 	int pin_pos;	//+ side node of the component.//for polar components
 	int pin_neg;	//same
-	string name;
+	int index;		//index on it's vector
 	bool log_on;	//true if you need to print component properties in run time
 
+	//index shift
+	int gm_shift, bm_shift, cm_shift, dm_shift, zm_shift, xm_shift;
 
 	//to be calculated value
-	float voltage_accross;	
-	float current_through;
+	double voltage_accross;
+	double current_through;
 
 	Component()
 	{
@@ -32,9 +39,10 @@ public:
 		current_through = 0;
 		log_on = false;
 		name = "hello";
+		gm_shift = bm_shift = cm_shift = dm_shift = zm_shift = xm_shift = 0;
 	}
 
-	Component(string nam,float v, int p1, int p2)
+	Component(string nam, double v, int p1, int p2)
 	{
 		value = v;
 		pin_pos = p1;
@@ -43,40 +51,14 @@ public:
 		current_through = 0;
 		name = nam;
 		log_on = false;
+		gm_shift = bm_shift = cm_shift = dm_shift = zm_shift = xm_shift = 0;
 	}
 
-	void Toggle_log(bool k)
-	{
-		log_on = k;
-	}
-
-	//checks if the given node attached to positive or negative side
-
-	virtual float pin_polarity(int pin)
-	{
-		if (pin == pin_pos)return 1.00;
-		else if (pin == pin_neg)return -1.00;
-		else
-		{
-			return 0.00;
-		}
-	}
-	
-	//returns the other pin attatched to this component
-
-	int getOther(int in_pin)
-	{
-		int other_pin;
-		if (in_pin == pin_pos)other_pin = pin_neg;
-		else if (in_pin == pin_neg)other_pin = pin_pos;
-		else
-		{
-			cout << "Pin selection error!\n";
-			other_pin = -1;
-		}
-
-		return other_pin;
-	}
+	virtual void update_matrix_G(double** g) { return; }
+	virtual void update_matrix_B(double** b) { return; }
+	virtual void update_matrix_C(double** c) { return; }
+	virtual void update_matrix_D(double** d) { return; }
+	virtual void update_matrix_Z(double* z) { return; }
 
 	void Log()
 	{
@@ -89,9 +71,25 @@ public:
 
 class Resistor : public Component
 {
+
 public:
 
-	Resistor(string n, float v1, int a1, int a2) :Component(n, v1, a1, a2) {}
+	Resistor(string n, double v1, int a1, int a2) :Component(n, v1, a1, a2) {}
+
+	void update_matrix_G(double** g)
+	{
+		int i1 = pin_pos - 1;
+		int i2 = pin_neg - 1;
+
+		if(i1 >= 0)g[i1][i1] += 1 / value;
+		if (i2 >= 0)g[i2][i2] += 1 / value;
+
+		if (i1 >= 0 && i2 >= 0)
+		{
+			g[i1][i2] -= 1 / value;
+			g[i2][i1] -= 1 / value;
+		}
+	}
 };
 
 //Ideal independent Voltage source
@@ -100,15 +98,40 @@ class Voltage : public Component
 {
 public:
 
-	Voltage(string n, float v1, int a1, int a2) :Component(n, v1, a1, a2){}
+	Voltage(string n, double v1, int a1, int a2) :Component(n, v1, a1, a2){}
+
+	void update_matrix_B(double** b)
+	{
+		if (pin_pos > 0)b[pin_pos - 1][index] += 1;
+		if (pin_neg > 0)b[pin_neg - 1][index] += -1;
+	}
+
+	void update_matrix_C(double** c)
+	{
+		if (pin_pos > 0)c[index][pin_pos - 1] += 1;
+		if (pin_neg > 0)c[index][pin_neg - 1] += -1;
+	}
+
+	void update_matrix_Z(double* z)
+	{
+		z[index+zm_shift] += value;
+	}
+
 };
 
 class Current : public Component
 {
 public:
 
-	Current(string n, float v1, int a1, int a2) :Component(n, v1, a1, a2){}
+	Current(string n, double v1, int a1, int a2) :Component(n, v1, a1, a2){}
+
+	void update_matrix_Z(double* z)
+	{
+		if (pin_pos > 0)z[pin_pos - 1] -= value;
+		if (pin_neg > 0)z[pin_neg - 1]+= value;
+	}
 };
+
 
 class Vdvs : public Component
 {
@@ -117,121 +140,82 @@ public:
 
 	int cpin_pos;
 	int cpin_neg;
-	float gain;
 
-	Vdvs(string n, float v, int p1, int p2, int cp1, int cp2)
+	Vdvs(string n, double v, int p1, int p2, int cp1, int cp2):Component(n,v,p1,p2)
 	{
-		gain = v;
-		value = 0;
-		pin_pos = p1;
-		pin_neg = p2;
-		voltage_accross = 0;
-		current_through = 0;
 		cpin_pos = cp1;
 		cpin_neg = cp2;
-		log_on = false;
 	}
 
-	float pin_polarity(int pin)
+	void update_matrix_C(double** c)
 	{
-		float polarity = 0;
-		polarity = pin == pin_pos ? 1.0 : pin == pin_neg ? -1.0 : 0.0;
-		if (pin == cpin_neg)polarity += gain;
-		else if (pin == cpin_pos)polarity -= gain;
-		return polarity;
+		if (cpin_pos > 0)c[cm_shift + index][cpin_pos - 1] -= value;
+		if(cpin_neg > 0)c[cm_shift + index][cpin_neg - 1] += value;
 	}
-
-	float pin_polarity(int pin, bool k)
-	{
-		if (pin == pin_pos)return 1.00;
-		else if (pin == pin_neg)return -1.00;
-		else
-		{
-			return 0.00;
-		}
-	}
-
 };
 
-class Nodes
+class Vdcs : public Component
 {
 public:
 
-	vector<Resistor> rs;
-	vector<Voltage> vs;
-	vector<Current> is;
-	vector<Vdvs> vdvss;
-	float node_voltage;
-	bool log_on;
+	int cpin_pos;
+	int cpin_neg;
 
-	Nodes()
+	Vdcs(string n, double v, int p1, int p2, int cp1, int cp2):Component(n,v,p1,p2)
 	{
-		//cout << "Creating node\n";
-		log_on = false;
-		node_voltage = 0;
+		cpin_pos = cp1;
+		cpin_neg = cp2;
 	}
 
-
-	void add_element(Resistor r)
+	void update_matrix_G(double** g)
 	{
-		rs.push_back(r);
-	}
-
-	void add_element(Voltage v)
-	{
-		vs.push_back(v);
-	}
-
-	void add_element(Current i)
-	{
-		is.push_back(i);
-	}
-
-	void add_element(Vdvs v)
-	{
-		vdvss.push_back(v);
-	}
-
-	void log()
-	{
-		if (log_on) {
-			cout << ".............\n";
-			for (unsigned i = 0; i < rs.size(); i++)
-			{
-				rs.at(i).Log();
-			}
-
-			for (unsigned i = 0; i < vs.size(); i++)
-			{
-				vs.at(i).Log();
-			}
-
-			for (int i = 0; i < (int)is.size(); i++)
-			{
-				is.at(i).Log();
-			}
-
-		}
-	}
-
-	float r_inverse()
-	{
-		float res = 0.0;
-		for (int i = 0; i < (int)rs.size(); i++)
+		if (pin_pos > 0)
 		{
-			res += 1 / rs.at(i).value;
+			if (cpin_pos > 0)g[pin_pos - 1][cpin_pos - 1] += value;
+			if (cpin_neg > 0)g[pin_pos - 1][cpin_neg - 1] -= value;
 		}
-		return res;
-	}
-
-	float total_current(int pin)
-	{
-		float i = 0;
-		for (int j = 0; j < (int)is.size(); j++)
+		if (pin_neg > 0)
 		{
-			i -= is.at(j).pin_polarity(pin) * is.at(j).value;
+			if (cpin_pos > 0)g[pin_neg - 1][cpin_pos - 1] -= value;
+			if (cpin_neg > 0)g[pin_neg - 1][cpin_neg - 1] += value;
 		}
-		return i;
+	}
+};
+
+class Capacitor : public Component
+{
+
+public:
+
+	double ic;		//initial condition.For capacitor, it's initial voltage
+	Capacitor(string n, double v1, int a1, int a2, double icond) :Component(n, v1, a1, a2)
+	{
+		ic = icond;
+		voltage_accross = ic;
 	}
 
+	void update_matrix_B(double** b)
+	{
+		if (pin_pos > 0)b[pin_pos - 1][index+bm_shift] += 1;
+		if (pin_neg > 0)b[pin_neg - 1][index+bm_shift] += -1;
+	}
+
+	void update_matrix_C(double** c)
+	{
+		if (pin_pos > 0)c[index+cm_shift][pin_pos - 1] += 1;
+		if (pin_neg > 0)c[index+cm_shift][pin_neg - 1] += -1;
+	}
+
+	void update_matrix_Z(double* z)
+	{
+		z[index + zm_shift] = voltage_accross;
+	}
+
+	// val =  i * delta_T
+	void update_values(double val)
+	{
+		double dv = (val / value);
+		voltage_accross += dv;
+		//cout <<" + " << dv <<" = "<<voltage_accross<<endl;
+	}
 };
