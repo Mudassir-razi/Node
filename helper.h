aff21ac82de506be0cea8f3072590d9a91595f04
Mudassir-razi <54622087+Mudassir-razi@users.noreply.github.com>
2020-06-08 17:01:34 +0600

    Update helper.h
    Function of helper is changed very much.It not just creates components, it assigns them properties and coordinates how each of them interact with matrices.

--- helper.h
+++ helper.h
@@ -8,25 +8,43 @@ using namespace std;
 
 //splitting string into segments
 
-
 class Helper {
+
+public:
 
 	vector<Resistor> *res;
 	vector<Voltage> *ves;
 	vector<Current> *ies;
-	vector<Nodes>* nos;
+	vector<double>* nos;
 	vector<Vdvs>* vdvses;
+	vector<Vdcs>* vdcses;
+	vector<Capacitor>* capses;
 
 public:
 
+	Helper()
+	{
+		nos = nullptr;
+		res = nullptr;
+		ves = nullptr;
+		ies = nullptr;
+		vdvses = nullptr;
+		vdcses = nullptr;
+		capses = nullptr;
+	}
+
 	//storing pointers 
-	Helper(vector<Nodes> *nos_var,vector<Resistor> *res_var, vector<Voltage> *ves_var, vector<Current> *ies_var, vector<Vdvs> *vdvs_var)
+	Helper(vector<double> *nos_var, vector<Resistor> *res_var, vector<Voltage> *ves_var,
+		   vector<Current> *ies_var, vector<Vdvs> *vdvs_var, vector<Vdcs> *vdcs_var,
+		   vector<Capacitor>* capses_var)
 	{
 		nos = nos_var;
 		res = res_var;
 		ves = ves_var;
 		ies = ies_var;
 		vdvses = vdvs_var;
+		vdcses = vdcs_var;
+		capses = capses_var;
 	}
 
 	void Update(string s)
@@ -34,7 +52,7 @@ public:
 		string name;
 		string vals;
 
-		vector<float> values;
+		vector<double> values;
 
 		for (int i = 0; i < (int)s.size(); i++)
 		{
@@ -64,8 +82,8 @@ public:
 			}
 
 			Resistor r(name, values.at(2), p1, p2);
+			r.index = res->size();
 			res->push_back(r);
-			update_info<Resistor>(r, p1, p2);	
 		}
 
 		else if (name[0] == 'V' || name[0] == 'v')
@@ -77,8 +95,8 @@ public:
 			}
 
 			Voltage r(name, values.at(2), p1, p2);
+			r.index = ves->size();
 			ves->push_back(r);
-			update_info(r, p1, p2);
 		}
 
 		else if (name[0] == 'I' || name[0] == 'i')
@@ -90,8 +108,8 @@ public:
 			}
 
 			Current r(name, values.at(2), p1, p2);
+			r.index = ies->size();
 			ies->push_back(r);
-			update_info(r, p1, p2);
 		}
 
 		else if (name[0] == 'E' || name[0] == 'e')
@@ -103,8 +121,34 @@ public:
 			}
 
 			Vdvs r(name, values.at(2), p1, p2, (int)values.at(2),(int)values.at(3));
+			r.index = vdvses->size();
 			vdvses->push_back(r);
-			update_info(r, p1, p2);
+		}
+
+		else if (name[0] == 'G' || name[0] == 'g')
+		{
+			if (values.size() != 5)
+			{
+				//Exception handle;
+				cout << "Error: Not enough info!!" << endl;
+			}
+
+			Vdcs r(name, values.at(2), p1, p2, (int)values.at(2), (int)values.at(3));
+			r.index = vdcses->size();
+			vdcses->push_back(r);
+		}
+
+		else if (name[0] == 'C' || name[0] == 'c')
+		{
+			if (values.size() != 4)
+			{
+				//Exception handle;
+				cout << "Error: Not enough info!!" << endl;
+			}
+
+			Capacitor r(name, values.at(2), p1, p2, values.at(3));
+			r.index = vdcses->size();
+			capses->push_back(r);
 		}
 
 		else
@@ -113,25 +157,89 @@ public:
 			//Exception handle
 			cout << "Error: No matching component found!!" << endl;
 		}
-
+		
+		update_node(p1, p2);
 	}
 	
+	//For printing matrix:
+
+	void log_matrix(double** m, int size)
+	{
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = 0; j < size; j++)
+			{
+				cout << std::setw(5) << m[i][j] << "  ";
+			}
+			cout << endl;
+		}
+	}
+
+	void log_matrix(double** m, int sizex, int sizey)
+	{
+		for (int i = 0; i < sizex; i++)
+		{
+			for (int j = 0; j < sizey; j++)
+			{
+				cout << std::setw(5) << m[i][j] << "  ";
+			}
+			cout << endl;
+		}
+	}
+
+	void log_matrix(double* m, int size)
+	{
+		for (int i = 0; i < size; i++)
+		{
+			cout << m[i] << endl;
+		}
+	}
+
 	void list_components()
 	{
 		print_element(res);
 		print_element(ves);
 		print_element(ies);
 		print_element(vdvses);
+		print_element(vdcses);
+		print_element(capses);
+	}
+
+	//Corrects the index of components
+	//Edit this according to README.md
+	//*********...............................................***********
+	void update_index()
+	{
+		shift_index<Voltage>(ves, 0, 0, 0, 0, (int)nos->size()-1,0);
+		shift_index<Vdvs>(vdvses, 0, 0, (int)ves->size(), 0, 0, 0);
+
+		int shift = (int)vdvses->size() + (int)ves->size();
+		shift_index<Capacitor>(capses, 0, shift, shift, 0, (int)nos->size() + shift - 1, (int)nos->size() + shift - 1);
 	}
 
 private:
 
-	vector<float> split_info(string s)
+	template <typename T>
+	void shift_index(vector<T>* obj,int gshift, int bshift, int cshift, int dshift, int zshift, int xshift)
+	{
+		int len = obj->size();
+		for (int i = 0; i < len; i++)
+		{
+			obj->at(i).gm_shift += gshift;
+			obj->at(i).bm_shift += bshift;
+			obj->at(i).cm_shift += cshift;
+			obj->at(i).dm_shift += dshift;
+			obj->at(i).zm_shift += zshift;
+			obj->at(i).xm_shift += xshift;
+		}
+	}
+
+	vector<double> split_info(string s)
 	{
 		int len = (int)s.size();
 		int last_step = 0;
-		float temp_val;
-		vector<float> values;
+		double temp_val;
+		vector<double> values;
 		string sub;
 		for (int i = 0; i < len; i++)
 		{
@@ -152,18 +260,13 @@ private:
 		return values;
 	}
 
-	template <typename T>
-	void update_info(T comp, int p1, int p2)
+	void update_node(int p1, int p2)
 	{
-		
-		int last_node = nos->size()-1;
 		int greater = p1 > p2 ? p1 : p2;
-		if (greater > last_node)
+		if ((int)nos->size() - 1 < greater)
 		{
-			nos->resize(greater+1);
+			nos->resize(greater + 1);
 		}
-		nos->at(p1).add_element(comp);
-		nos->at(p2).add_element(comp);
 	}
 
 	template <typename T>
